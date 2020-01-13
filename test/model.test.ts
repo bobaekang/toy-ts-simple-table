b070f4710f31c842d7bb5147701e93f5ded146bc
Bobae Kang <bkang.kr@gmail.com>
2020-01-13 16:53:18 -0600

    simplify sampleTableDTO

--- test/model.test.ts
+++ test/model.test.ts
@@ -43,26 +43,10 @@ const sampleTable: Table = [
 ]
 
 const sampleTableDTO: TableDTO = [
-  {
-    A: toInt(1),
-    B: toInt(1),
-    value: toInt(1),
-  },
-  {
-    A: toInt(1),
-    B: toInt(2),
-    value: toInt(2),
-  },
-  {
-    A: toInt(2),
-    B: toInt(1),
-    value: toInt(3),
-  },
-  {
-    A: toInt(2),
-    B: toInt(2),
-    value: toInt(4),
-  },
+  { A: 1, B: 1, value: 1 },
+  { A: 1, B: 2, value: 2 },
+  { A: 2, B: 1, value: 3 },
+  { A: 2, B: 2, value: 4 },
 ]
 
 describe('filter', () => {
