bba13bc453558c5a4562759f81895456b45a1589
Bobae Kang <bkang.kr@gmail.com>
2020-01-29 17:42:49 -0600

    delete Int type since no longer needed

--- src/int.ts
+++
@@ -1,6 +0,0 @@
-// https://spin.atomicobject.com/2018/11/05/using-an-int-type-in-typescript/
-export type Int = number & { __int__: void }
-
-export function toInt(value: string | number): Int {
-  return Number.parseInt(value.toString()) as Int
-}
